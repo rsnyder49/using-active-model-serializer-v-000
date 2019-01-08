0e0c73e9b7aa520c59287b683a03beb7938ea700
Robert Snyder <r.snyder49@gmail.com>
2019-01-08 21:35:21 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -28,7 +28,7 @@ describe 'form' do
     fill_in 'Title', with: "My post title"
     fill_in 'Description', with: "My post description"
 
-    click_on "Submit Post"
+    click_on "Create Post"
 
     expect(page).to have_content("My post title")
   end
